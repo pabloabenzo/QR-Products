ef91f27f80c09aa851b3534ffce2789d4b296717
Pablo Alejandro <pabloabenzo@hotmail.com>
2025-06-19 21:58:50 -0300

    Initial Commit

---
+++ QR_ProductsUITests/QR_ProductsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  QR_ProductsUITestsLaunchTests.swift
+//  QR_ProductsUITests
+//
+//  Created by Pablo Benzo on 19/06/2025.
+//
+
+import XCTest
+
+final class QR_ProductsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
